3f6fc472f727e9f7d0fb6f549ffb54c02114dcdd
Vikash Sharma <82801726+vikash-ibm@users.noreply.github.com>
2023-01-18 12:01:37 +0530

    Update refresh.py

--- SoftLayer/CLI/block/refresh.py
+++ SoftLayer/CLI/block/refresh.py
@@ -9,10 +9,14 @@ from SoftLayer.CLI import environment
 @click.command(cls=SoftLayer.CLI.command.SLCommand, )
 @click.argument('volume_id')
 @click.argument('snapshot_id')
+@click.option('--force-refresh', '-f',
+    help="An optional parameter for force refreshing the volume")
+    type=click.Choice(['True', 'False']),
+    default='False',
 @environment.pass_env
-def cli(env, volume_id, snapshot_id):
+def cli(env, volume_id, snapshot_id, force_refresh):
     """Refresh a duplicate volume with a snapshot from its parent."""
     block_manager = SoftLayer.BlockStorageManager(env.client)
-    resp = block_manager.refresh_dupe(volume_id, snapshot_id)
+    resp = block_manager.refresh_dupe(volume_id, snapshot_id, force_refresh)
 
     click.echo(resp)
